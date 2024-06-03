033d30240b6a78a3e2f7308f4e7a999bfb89701b
José Fernando <jfengineer935@gmail.com>
2024-06-03 17:45:56 +0000

    Linear index search

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## Algorithms
-1. [Bubble Sort](/sort/main.c)
-2. [Linear Search](/linearSearch/main.cpp)
+- Sort
+    1. [Bubble Sort](/sort/main.c)
+- Search
+    1. [Linear Search](/linearSearch/main.cpp)
+    2. [Index Linear Search](/indexLinearSearch/main.cpp)
+    3. [Binary Search](/linearSearch/main.cpp)
+
 
 ## Mathematical theorems
 1. [Leibniz](/leibniz/main.c)
--- linearSearch/main.cpp
+++ linearSearch/main.cpp
@@ -9,22 +9,17 @@ int linearSearch(int num, const int array[], int size){
 
     for(int i = 0; i < size; i++){
         if(num == array[i]){
-            cout << "The number is in " << i + 1 << "º position \n";
             return i;
         }
     }
-    cout << "Your number is not in the array\n";
     return -1;
 }
 
 
 int main(){
     const int size = 30;
-    int arr[] = {5, 20, 12, 8, 6, 28, 23, 15, 29, 4, 9, 7, 16, 10, 30, 27, 11, 25, 17, 19, 1, 14, 2, 22, 3, 24, 18, 21, 26, 13};
-    
-    int number;
-    cout << "Insert one number beetwen 1 and 50: ";
-    cin >> number;
+    int arr[] = {5, 20, 12, 8, 6, 28, 23, 15, 29, 4, 9, 7, 16, 10, 30, 27, 11, 25, 17, 19, 1, 14, 2, 22, 3, 24, 18, 21, 26, 13};    
+    int number = 8;
 
     linearSearch(number, arr, size);
 
